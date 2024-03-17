3e9c042c29480968c658b84b0dd0da035757f12b
latif <latifyousuf513@gmail.com>
2024-03-17 20:58:26 +0500

    commit

--- server.js
+++ server.js
@@ -25,12 +25,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'))
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
 
 //routes
 app.use("/api/v1/user", userRoutes);
